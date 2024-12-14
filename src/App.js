b623f43afb79261e6f1d41e87c9f820fe0d2476a
vijaykumar <hexaexplorers@gmail.com>
2024-12-15 00:12:31 +0530

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 
-function App() {
+import './styles.css';
+import VideoBg from '../src/assets/video.mp4'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <section className='page'>
+    <div className='overlay'></div>
+    <video src={VideoBg} autoPlay loop muted></video>
+    <div className='page_content'>
+      <h1>Coming Soon </h1>
+      <h1>MS Enterprises India</h1>
+      <h3>Leave your email and we'all let you know once the site goes live</h3>
+      <FlipClockCountdown
+       className='flip-clock'
+        to={new Date().getTime() + 86 * 4600 * 1000 + 5000}
+        labels={['DAYS', 'HOURS', 'MINUTES', 'SECONDS']}
+        duration={0.5}
+      >
+        Finished
+      </FlipClockCountdown>
+      <button className='btn'>Notify Me</button>
     </div>
-  );
-}
+  </section>);
+};
 
 export default App;
